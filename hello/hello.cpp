013e27967bb576a3bc008a14eef1a29df83abfe1
tjel <tomek.jelinski@gmail.com>
2015-10-07 23:06:25 +0200

    MBP info added.

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -7,6 +7,7 @@ int main()
 cout << "Hello World!" << endl;
 cout << "Hello World 2!" << endl;
 //cout << "Hello World! It's me, roman-optiplex." << endl;
+cout << "Hello World! MBP is better than roman-optiplex." << endl;
 //cout << "Hello World 4!" << endl;
 return 0;
 }
