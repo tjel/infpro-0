aeda96e3ffa074e0a700b79a27f2be218de094c8
tjel <tomek.jelinski@gmail.com>
2015-10-07 22:27:42 +0200

    v1 of hello.cpp added.

---
+++ hello/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+cout << "Hello World!" << endl;
+//cout << "Hello World 2!" << endl;
+//cout << "Hello World 3!" << endl;
+//cout << "Hello World 4!" << endl;
+return 0;
+}
