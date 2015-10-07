caa34c5c8d231cd4f892b06b89418a488d1aaa2a
tjel <tomek.jelinski@gmail.com>
2015-10-07 23:03:22 +0200

    roman's welcome commented.

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -6,7 +6,7 @@ int main()
 {
 cout << "Hello World!" << endl;
 cout << "Hello World 2!" << endl;
-cout << "Hello World! It's me, roman-optiplex." << endl;
+//cout << "Hello World! It's me, roman-optiplex." << endl;
 //cout << "Hello World 4!" << endl;
 return 0;
 }
