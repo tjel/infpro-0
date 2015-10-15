e514be4632f777dc94b53285faddc1412410d0e3
roman-optiplex <roman.optiplex@gmail.com>
2015-10-15 02:21:43 +0200

    Add space in the prompted message.

--- area/area.cpp
+++ area/area.cpp
@@ -6,7 +6,7 @@ int main( )
 { 
     float PI = 3.141592;                // variables can be initialized during declaration 
     double rad; 
-    cout<< "Enter the radius"; 
+    cout<< "Enter the radius: "; 
     cin>>rad; 
     cout<< "Area of the circle is "<< PI * rad * rad; 
     return 0;
